17e220416b0dc3bcfd6054d3fff9551f8863fe86
AnDev77 <dnr0627@naver.com>
2024-08-25 17:54:01 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+let http = require('http');
+let url = require('url');
+
+function start(route, handle){
+    function onRequest(request, response) {
+
+        let pathname = url.parse(request.url).pathname;
+        let queryData = url.parse(request.url, true).query;
+
+        if (pathname == 'favicon.ico'){
+            response.writeHead(200, {'Content-Type' : 'text/html'});
+            response.end();
+            return
+        }
+        
+        route(pathname, handle, response, queryData.productId);
+    }
+
+    http.createServer(onRequest).listen(8888);
+}
+
+
+exports.start = start;
